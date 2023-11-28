2be4eeac4e574d6cf68f56279049ce02e37c6a40
sandraldr27 <sandralilder@gmail.com>
2023-11-28 19:58:37 +0100

    A fetch is made from an external API

--- src/api/fetchCharacters.jsx
+++ src/api/fetchCharacters.jsx
@@ -1,2 +1,11 @@
 {/*URL desde donde haré la llamada a la API: https://dummyjson.com/users*/}
 
+export async function fetchCharacters() {
+    try {
+        const response = await fetch("https://dummyjson.com/users");
+        const results = await response.json();
+    return results.users;
+        } catch (error) {
+            console.error(error);
+        }
+}
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,17 +1,39 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useEffect } from 'react'; {/*Importamos useEffect de react*/}
+import { useEffect } from 'react'; //Importamos useEffect de react//
+import { fetchCharacters } from '../api/fetchCharacters';
+import { useState } from 'react';
+
 
 const Characters = () => {
+    const [Characters, setCharacters] = useState([]);
 
     useEffect(() => {
-        {/* Aquí irá el fetch de nuestra super API */}
-        console.log("Voy a hacer un Fetch"); {/*Añado un console.log de prueba*/}
+            // Aquí irá el fetch de la API //
+            const getCharacters = async () => {
+                const data = await fetchCharacters();
+                console.log(data);
+                setCharacters(data);
+            };
+            getCharacters();
         }, []);
 
-    return (
-        <div>
-            
+        return (
+            <div>
+                {Characters ? (
+                    <ul>
+                        {Characters.map((character) => (
+                            <li key={character.id}>
+                                <img src={character.image} alt="" height="200px" width="200px" />
+                                <div>
+                                    {character.firstName} {character.age}{" "}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Loading characters</p>
+            )}
         </div>
     );
 };
